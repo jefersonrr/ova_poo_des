1594928c8fa3321e36b2cb71c68f51dfad079e34
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2022-12-19 19:31:15 -0500

    Validación de inputs en el formu de registrar

--- apps/tests/models.py
+++ apps/tests/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 class Tests(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(null=False,blank=False,max_length=400)
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return  self.name
@@ -18,7 +18,7 @@ class IntentoTest(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     test = models.ForeignKey(Tests,on_delete=models.CASCADE)
     score_total = models.FloatField(blank=False, null=False)
-    date = models.DateField(blank=False,null=False, default=timezone.now())
+    date = models.DateField(blank=False,null=False, default=timezone.now)
     
     def __str__(self):
         return str(self.id)
